db5e8f90137062ed432e3ec8288e2a18c7b217c6
Albert <albert@alberts-mac.local>
2017-11-09 11:47:13 +0100

    Initial Commit

---
+++ Dropit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dropit
+//
+//  Created by Albert on 09/11/17.
+//  Copyright © 2017 Albert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dropit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dropit
+//
+//  Created by Albert on 09/11/17.
+//  Copyright © 2017 Albert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
